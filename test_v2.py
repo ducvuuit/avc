84bb8aaa04bb72944c806360d50d3acd58ce126c
CS114.K21.KHTN <62535444+ducvuuit@users.noreply.github.com>
2022-03-22 11:04:17 +0700

    Update test_v2.py

--- test_v2.py
+++ test_v2.py
@@ -1,4 +1,8 @@
 """
     What are you doing?
     I've just hang out with my friend.
+    What are you doing?
+    I've just hang out with my friend.
+    What are you doing?
+    I've just hang out with my friend.
 """
